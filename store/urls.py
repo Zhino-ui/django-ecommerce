81ec4aea245f84199d89a7661ace40fada20d4b0
Maxwell Asante <zhino4u2010@gmail.com>
2023-09-29 14:48:25 +0000

    django-ecommerce-1

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('store/', views.store, name='store'),
+    path('store/<slug:category_slug>', views.store, name='product_by_category'),
+    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
