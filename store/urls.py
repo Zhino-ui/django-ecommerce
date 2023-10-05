f2668e50912ffd3459cc4089f0ae2f935f59ca94
Maxwell Asante <zhino4u2010@gmail.com>
2023-10-05 16:16:47 +0000

    search functionality completed

--- store/urls.py
+++ store/urls.py
@@ -9,6 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('store/', views.store, name='store'),
-    path('store/<slug:category_slug>', views.store, name='product_by_category'),
-    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('store/category/<slug:category_slug>', views.store, name='product_by_category'),
+    path('store/category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('store/search/', views.search, name='search')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
