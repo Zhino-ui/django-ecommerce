fef3089681d371141162515863944bb99d079298
Maxwell Asante <zhino4u2010@gmail.com>
2023-11-01 15:20:13 +0000

    product order and payments

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'carts',
+    'orders',
 ]
 
 MIDDLEWARE = [
--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', include('store.urls')),
     path('cart/', include('carts.urls')),
     path('accounts/', include('accounts.urls')),
+    path('orders/', include('orders.urls')),
 ]
