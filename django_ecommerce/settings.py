fcd1042f5ebe4b7546c8f01aef8083cd5a6499a3
Maxwell Asante <zhino4u2010@gmail.com>
2023-11-30 14:19:55 +0000

    allowed host

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-project-env.eba-3h3tzb8q.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-dev-env.eba-2x3tb2qm.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
